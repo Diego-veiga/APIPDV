ad574ccedd3b7e039a8f68cb1d1356eb8d8e3a91
DiegoVeiga <diegoroberto27@hotmail.com>
2021-08-05 22:43:38 -0300

    adiconando validacoes com joi

--- src/router/tokenRouter.js
+++ src/router/tokenRouter.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import tokenController from '../controller/tokenController';
+import validateRequest from '../middleware/validateRequest';
+import loginSchema from '../schemas/loginSchema';
 
 const router = new Router();
 
-router.post('/', tokenController.store);
+router.post('/', validateRequest(loginSchema, 'body'), tokenController.store);
 
 export default router;
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import userController from '../controller/userController';
 import tokenRequired from '../middleware/tokenRequired';
+import validateRequest from '../middleware/validateRequest';
+import userSchemas from '../schemas/userSchema';
 
 const router = new Router();
 
-router.post('/', tokenRequired, userController.store);
+router.post('/', validateRequest(userSchemas.post, 'body'), tokenRequired, userController.store);
 router.get('/', tokenRequired, userController.index);
-router.get('/:id', tokenRequired, userController.show);
-router.put('/:id', tokenRequired, userController.update);
-router.delete('/:id', tokenRequired, userController.delete);
+router.get('/:id', validateRequest(userSchemas.get, 'params'), tokenRequired, userController.show);
+router.put('/:id', validateRequest(userSchemas.get, 'params'), validateRequest(userSchemas.update, 'body'), tokenRequired, userController.update);
+router.delete('/:id', validateRequest(userSchemas.get, 'params'), tokenRequired, userController.delete);
 
 export default router;
